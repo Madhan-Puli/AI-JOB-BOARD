112001dae734b841456e5fae5988971a8eadc68e
Puli Madhan <madhanstyle571@gmail.com>
2026-07-03 22:24:09 +0530

    Connect project to Railway MySQL

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,11 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Madhan@123",
-  database: "jobboard",
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: Number(process.env.MYSQLPORT),
 });
 
 export default db;
