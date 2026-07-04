396e5c8b7605bbf315da0eac3620670462a7dad1
Puli Madhan <madhanstyle571@gmail.com>
2026-07-04 17:37:21 +0530

    Fix MongoDB env issue in CI build

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -2,20 +2,12 @@ import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
 
-if (!uri) throw new Error("Missing MONGODB_URI");
+if (!uri) {
+  throw new Error("Missing MONGODB_URI");
+}
 
 const client = new MongoClient(uri);
 
-let clientPromise: Promise<MongoClient>;
-
-declare global {
-  var _mongo: Promise<MongoClient> | undefined;
-}
-
-if (!global._mongo) {
-  global._mongo = client.connect();
-}
-
-clientPromise = global._mongo;
+const clientPromise = client.connect();
 
 export default clientPromise;
